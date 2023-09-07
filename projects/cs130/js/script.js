5150e7dbbd4c508f12bdc422dd8a0e047fe19dfe
Sabina Dahal <dahalsabina@Sabinas-Air.lc.luther.edu>
2023-09-07 12:48:12 -0500

    Add cs 130

---
+++ projects/cs130/js/script.js
@@ -0,0 +1,21 @@
+//
+//window.alert('hello')
+
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+function myFunction() {
+  document.getElementById("demo").style.fontSize = "25px"; 
+  document.getElementById("demo").style.color = "red";
+  document.getElementById("demo").style.backgroundColor = "yellow";        
+}
+
+
